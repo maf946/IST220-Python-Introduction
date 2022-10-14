a7cd6d91de137bce6bb9ef0dcd0182845b699499
Marc Friedenberg <maf946@psu.edu>
2022-10-14 09:36:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ def print_x(text_to_print, number_of_times):
         print(text_to_print)
 
 name = input("What's your name? ")
-print_x("All work and no play makes " + name + " a dull student.", 10)
+print_x("All work and no play makes " + name + " a dull student.", 5)
