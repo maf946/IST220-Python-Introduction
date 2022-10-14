fde9c9c6b6df25db7e31dcb78b53d622e7fca627
Marc Friedenberg <maf946@psu.edu>
2022-10-14 09:33:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,4 @@ def print_x(text_to_print, number_of_times):
         print(text_to_print)
 
 name = input("What's your name? ")
-dreamJob = input(("Hey there, " + name + ". What was your dream job you envisioned when you were a little kid? "))
-
-print_x("Never forget that " + name + " still has time to achieve their dream job: " + dreamJob, 5)
+print_x("All work and no play makes " + name + " a dull student.", 10)
